dc0d2a6d193ed73de935ab4bd08483634caa4d87
Jan Zelinsky <Jan.Zelinsky@it.htl-hl.ac.at>
2026-02-20 09:30:40 +0100

    edited create assignment view

--- views/assignments/_form.php
+++ views/assignments/_form.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\bootstrap5\ActiveForm;
 
 /** @var yii\web\View $this */
 /** @var app\models\Assignments $model */
@@ -16,13 +16,13 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'isCompleted')->textInput() ?>
+    <?= $form->field($model, 'isCompleted')->checkbox(['style' => 'transform: scale(1.5);']) ?>
 
-    <?= $form->field($model, 'due_date')->textInput() ?>
+    <?= $form->field($model, 'due_date')->input('date', ['class' => 'form-control']) ?>
 
-    <?= $form->field($model, 'userID')->textInput() ?>
+    <?= $form->field($model, 'userID')->hiddenInput(['value' => 0])->label(false) ?>
 
-    <?= $form->field($model, 'subjectID')->textInput() ?>
+    <?= $form->field($model, 'subjectID')->hiddenInput(['value' => 0])->label(false) ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
--- views/assignments/index.php
+++ views/assignments/index.php
@@ -10,18 +10,12 @@ use yii\grid\GridView;
 /** @var app\models\AssignmentsSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = Yii::t('app', 'Assignments');
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="assignments-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
         <?= Html::a(Yii::t('app', 'Create Assignments'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
