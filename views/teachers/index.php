3ae567894282367fd00c087580e8eae5966e965c
Elisabeth Hengstberger <Elisabeth.Hengstberger@it.htl-hl.ac.at>
2026-03-02 10:35:42 +0100

    added button for viewing all teachers, added methode to delete items bc of zwischentabellen, better view for teachers

--- views/teachers/index.php
+++ views/teachers/index.php
@@ -9,6 +9,8 @@ use yii\grid\GridView;
 /** @var yii\web\View $this */
 /** @var app\models\TeachersSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
+/** @var \app\models\Teachers[] $teachers */
+
 
 $this->title = Yii::t('app', 'Teachers');
 ?>
@@ -20,25 +22,30 @@ $this->title = Yii::t('app', 'Teachers');
         <?= Html::a(Yii::t('app', 'Create Teachers'), ['create'], ['class' => 'btn btn-outline-secondary']) ?>
     </p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'teacherID',
-            'teachername',
-            'isActive',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Teachers $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'teacherID' => $model->teacherID]);
-                 }
-            ],
-        ],
-    ]); ?>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Status</th>
+            <th scope="col">-</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($teachers as $index => $teacher): ?>
+            <tr class="<?= $teacher->isActive ? 'table-success' : 'table-danger' ?>">
+                <th scope="row"><?= $index + 1 ?></th>
+                <td><?= Html::encode($teacher->teachername) ?></td>
+                <td><?= $teacher->isActive ? 'active' : 'not active' ?></td>
+                <td>
+                    <?= \yii\bootstrap5\Html::a($teacher->editIconUpdate(), Url::to(['update', 'teacherID' => $teacher->teacherID]), ['class' => 'btn btn-outline-secondary btn-sm']) ?>
+                    <?= \yii\bootstrap5\Html::a($teacher->deleteIconUpdate(), Url::to(['delete', 'teacherID' => $teacher->teacherID]), ['class' => 'btn btn-outline-secondary btn-sm', 'data-confirm' => 'Möchtest du diesen Lehrer wirklich löschen?', 'data-method' => 'post',
+                    ]) ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
 
 
 </div>
